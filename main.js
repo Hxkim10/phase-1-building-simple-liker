5980fc65fdcf4c87ba16ad4c47ff81f03d39665f
hxkim10 <abdelhakimtiabi@gmail.com>
2023-10-01 07:21:27 +0100

    Assignment Completed

--- main.js
+++ main.js
@@ -4,22 +4,42 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+// Create a hidden modal element
+const modal = document.createElement('div');
+modal.classList.add('hidden');
+document.body.appendChild(modal);
 
+// Function to open the modal
+function openModal() {
+  modal.classList.remove('hidden');
+}
 
+// Function to close the modal
+function closeModal() {
+  modal.classList.add('hidden');
+}
+
+// Add a click listener to the "Open Modal" button
+const openModalButton = document.querySelector('#open-modal-button');
+openModalButton.addEventListener('click', openModal);
+
+// Add a click listener to the "Close Modal" button
+const closeModalButton = document.querySelector('#close-modal-button');
+closeModalButton.addEventListener('click', closeModal);
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
 function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
-      if (isRandomFailure) {
-        reject("Random server error. Try again.");
-      } else {
-        resolve("Pretend remote server notified of action!");
-      }
-    }, 300);
-  });
+ return new Promise(function(resolve, reject) {
+  setTimeout(function() {
+   let isRandomFailure = Math.random() < .2
+   if (isRandomFailure) {
+    reject("Random server error. Try again.");
+   } else {
+    resolve("Pretend remote server notified of action!");
+   }
+  }, 300);
+ });
 }
